1412f8e94c112c4fbc6939d60afd0b401cc370fe
Michael-Xie <michael.xie@touchless.inc>
2024-03-12 10:34:28 -0400

    feat: improve preview of event

--- schemaTypes/eventType.ts
+++ schemaTypes/eventType.ts
@@ -1,9 +1,11 @@
 import {defineField, defineType} from 'sanity'
+import {CalendarIcon} from '@sanity/icons'
 
 export const eventType = defineType({
   name: 'event',
   title: 'Event',
   type: 'document',
+  icon: CalendarIcon,
   groups: [
     {name: 'details', title: 'Details'},
     {name: 'editorial', title: 'Editorial'},
@@ -82,4 +84,32 @@ export const eventType = defineType({
       group: 'details',
     }),
   ],
+  // Update the preview key in the schema
+  preview: {
+    select: {
+      name: 'name',
+      venue: 'venue.name',
+      artist: 'headline.name',
+      date: 'date',
+      image: 'image',
+    },
+    prepare({name, venue, artist, date, image}) {
+      const nameFormatted = name || 'Untitled event'
+      const dateFormatted = date
+        ? new Date(date).toLocaleDateString(undefined, {
+            month: 'short',
+            day: 'numeric',
+            year: 'numeric',
+            hour: 'numeric',
+            minute: 'numeric',
+          })
+        : 'No date'
+
+      return {
+        title: artist ? `${nameFormatted} (${artist})` : nameFormatted,
+        subtitle: venue ? `${dateFormatted} at ${venue}` : dateFormatted,
+        media: image || CalendarIcon,
+      }
+    },
+  },
 })
