3dfdaa2d9bd59617c7e976cf5abd480d88c031e6
Michael-Xie <michael.xie@touchless.inc>
2024-03-14 10:53:27 -0400

    fix: add apiVersion to queries made in structure

--- structure/index.ts
+++ structure/index.ts
@@ -10,12 +10,19 @@ export const structure: StructureResolver = (S) =>
         .title('Upcoming Events')
         .schemaType('event')
         .icon(CalendarIcon)
-        .child(S.documentList().title('Upcoming Events').filter('date > now()')),
+        .child(
+          S.documentList()
+            .apiVersion('v2023-03-08')
+            .title('Upcoming Events')
+            .filter('date > now()'),
+        ),
       S.listItem()
         .title('Past Events')
         .schemaType('event')
         .icon(CalendarIcon)
-        .child(S.documentList().title('Past Events').filter('date < now()')),
+        .child(
+          S.documentList().apiVersion('v2023-03-08').title('Past Events').filter('date < now()'),
+        ),
       S.divider(),
       S.documentTypeListItem('artist').title('Artists').icon(UsersIcon),
       S.documentTypeListItem('venue').title('Venues').icon(PinIcon),
